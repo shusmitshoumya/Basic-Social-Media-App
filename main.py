09743e0fe0fff6615358b5267dc4167cfbf3fdc0
shusmitshoumya <shoumya23@gmail.com>
2025-07-10 11:20:09 +0600

    registration and login function updatted

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from customtkinter import *
+from user_reg import *
 
 root = CTk()
 #set_default_color_theme("blue")
@@ -13,50 +14,68 @@ root.resizable(0,0)
 
 
 def clear_frame():
+    
     for widgets in frame.winfo_children():
         widgets.destroy()
 
 
-def home_screen() :
+
+heading = Label(frame, text="Welcome to (App Name)", bg = "#1B3C53", fg="white", font="Roboto")
+heading.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+
+UserLabel = CTkLabel(master = frame, text='Username', width=40, height=28, fg_color='transparent')
+UserLabel.place(relx = 0.3, rely=0.4)
+
+userEntry = CTkEntry(master=frame, placeholder_text="Username", width=250)
+userEntry.place(relx = 0.4, rely =0.4)
+
+passwordLabel = CTkLabel(master = frame, text='Password', width=40, height=28, fg_color='transparent')
+passwordLabel.place(relx = 0.3, rely=0.5)
+
+passwordEntry = CTkEntry(master=frame, placeholder_text="Password", show = "*", width=250)
+passwordEntry.place(relx = 0.4, rely=0.5)
+
+msgLabel = CTkLabel(frame, text = "", text_color="#E60F0F")
+msgLabel.place(relx=0.42, rely=0.57)
 
 
-    heading = Label(frame, text="Welcome to (App Name)", bg = "#1B3C53", fg="white", font="Roboto")
-    heading.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+# Registration
+def register():
+    #pass
+    #clear_frame()
+    userName = userEntry.get()
+    password = passwordEntry.get()
 
-    UserLabel = CTkLabel(master = frame, text='Username', width=40, height=28, fg_color='transparent')
-    UserLabel.place(relx = 0.3, rely=0.4)
+    if userName == "" or password == "" :
+        msgLabel.config(text="Fields cannot be empty!")
+        return
 
-    userEntry = CTkEntry(master=frame, placeholder_text="Username", width=250)
-    userEntry.place(relx = 0.4, rely =0.4)
+    success, message = userRegister(userName, password)
+    msgLabel.config(text = message, fg = "green" if success else "orange")
 
-    passwordLabel = CTkLabel(master = frame, text='Password', width=40, height=28, fg_color='transparent')
-    passwordLabel.place(relx = 0.3, rely=0.5)
-
-    passwordEntry = CTkEntry(master=frame, placeholder_text="Password", show = "*", width=250)
-    passwordEntry.place(relx = 0.4, rely=0.5)
-
-    def register():
-
-        #pass
-        clear_frame()
-
-        userName = userEntry.get()
-        password = passwordEntry.get()
-
-    def login() :
-
-        clear_frame()
-
-        userName = userEntry.get()
-        password = passwordEntry.get()
-
-    loginBtn = CTkButton(master = frame, text='Log in', fg_color="#0D5EA6", command=login, corner_radius=30, height = 30) 
-    loginBtn.place(relx = 0.3, rely=0.65)
-
-    regBtn = CTkButton(master=frame, text="Register", fg_color="#0D5EA6", command=register, corner_radius=30, height = 30)
-    regBtn.place(relx=0.51, rely =0.65)
+#Login successfull hoile Feed e niye jabe, naile retry
+def login() :
+    #clear_frame()
+    userName = userEntry.get()
+    password = passwordEntry.get()
+    
+    if userLogin(userName, password) :
+        msgLabel.config(text = "Login Successful!", fg="green")
+        Home()
+    else :
+        msgLabel.config(text="Incorrect username or password. Please try again.", fg = "red")
 
 
-home_screen()
+loginBtn = CTkButton(master = frame, text='Log in', fg_color="#0D5EA6", command=login, corner_radius=30, height = 30) 
+loginBtn.place(relx = 0.3, rely=0.65)
 
+regBtn = CTkButton(master=frame, text="Register", fg_color="#0D5EA6", command=register, corner_radius=30, height = 30)
+regBtn.place(relx=0.51, rely =0.65)
+
+
+def Home():
+    #clear_frame()
+    pass
+
+    
 root.mainloop()
--- user_reg.py
+++ user_reg.py
@@ -1,2 +1,39 @@
 import sqlite3
+import hashlib
 
+conn  = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+cursor.execute(
+    '''CREATE TABLE IF NOT EXISTS users(    
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password NOT NULL
+    )'''
+)
+
+conn.commit()
+
+
+def hash_pass(password) :
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+def userRegister(username, password) :
+    try:
+        hashed_pass = hash_pass(password)
+        cursor.execute("INSERT INTO users(username, password) VALUES(?, ?)", (username, hashed_pass))
+        conn.commit()
+
+        return True, "User registered succesfully!"
+    
+    except sqlite3.IntegrityError :
+        return False, "Username already exists!"
+
+
+def userLogin(username, password) :
+        
+        hashed = hash_pass(password)
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+
+        return cursor.fetchone() is not None
